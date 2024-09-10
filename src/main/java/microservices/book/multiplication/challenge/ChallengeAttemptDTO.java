bb642e4aa773a4a13de2a24d1d55a68668c7d0fc
Ben <ben.allan5888@gmail.com>
2024-09-10 18:28:20 +0100

    Added ChallengeAttempt Controller and tests to validate, however something is incorrect with the response

--- src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptDTO.java
+++ src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptDTO.java
@@ -1,12 +1,16 @@
 package microservices.book.multiplication.challenge;
 
+import jakarta.validation.constraints.*;
 import lombok.Value;
 /**
  * Attempt coming from the User
  */
 @Value
 public class ChallengeAttemptDTO {
+     @Min(1) @Max(99)
      int factorA, factorB;
+     @NotBlank
      String userAlias;
+     @Positive
      int guess;
 }
